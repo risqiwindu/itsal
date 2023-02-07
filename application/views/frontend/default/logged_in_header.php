40dfa4557ae600d13df3e395109da1df4888ef47
risqiwindu <risqiwindu.h@gmail.com>
2023-02-08 06:30:52 +0700

    second commit

--- application/views/frontend/default/logged_in_header.php
+++ application/views/frontend/default/logged_in_header.php
@@ -36,11 +36,11 @@ $user_details = $this->user_model->get_user($this->session->userdata('user_id'))
                         </div>
                     <?php endif; ?>
 
-                    <div class="instructor-box menu-icon-box">
+                    <!-- <div class="instructor-box menu-icon-box">
                         <div class="icon">
                             <a href="<?php echo site_url('home/my_courses'); ?>" style="border: 1px solid transparent; margin: 10px 10px; font-size: 14px; width: 100%; border-radius: 0; min-width: 100px;"><?php echo site_phrase('my_courses'); ?></a>
                         </div>
-                    </div>
+                    </div> -->
 <!-- 
                     <div class="wishlist-box menu-icon-box" id="wishlist_items">
                         <?php include 'wishlist_items.php'; ?>
